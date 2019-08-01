3fbe2b55e43f704404ede03a949a9b8ebeb7b41a
Minong <minhk3259@gmail.com>
2019-08-01 13:38:32 +0900

    페이지 컴포넌트 구현

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { ReactNode, SFC } from 'react';
+import BackgroundText from '../BackgroundText/BackgroundText';
 
-const Page: React.FC = props => {
-  return <div className={"wrapper"}> {props.children} </div>;
+import styles from './Page.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  children: ReactNode;
+  id?: string;
+  current: string;
+  backgroundText?: string[];
+}
+
+const Page: SFC<Props> = ({ children, backgroundText = [], id, current }) => {
+  return (
+    <section className={cx('page')} id={id}>
+      {current === id && children}
+      {backgroundText && <BackgroundText texts={backgroundText} />}
+    </section>
+  );
 };
 
 export default Page;
