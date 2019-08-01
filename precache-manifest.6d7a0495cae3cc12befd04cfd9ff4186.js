2c851540d523fe981a16eaab19a9635508aee4fd
Minong <minhk3259@gmail.com>
2019-08-01 14:19:24 +0900

    Updates

---
+++ precache-manifest.6d7a0495cae3cc12befd04cfd9ff4186.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28abbc971899ba5606d9a4f3d3161eff",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d613346a8739a8824374",
+    "url": "/react-portfolio/static/css/main.ea0451c6.chunk.css"
+  },
+  {
+    "revision": "de52dc4ca9b8a32eaca8",
+    "url": "/react-portfolio/static/js/2.3a574b9a.chunk.js"
+  },
+  {
+    "revision": "d613346a8739a8824374",
+    "url": "/react-portfolio/static/js/main.f0fd7bc5.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "475b539094d914ff68e420b4571c96c3",
+    "url": "/react-portfolio/static/media/TextMeOne-Regular.475b5390.ttf"
+  },
+  {
+    "revision": "606ea0c8fb99b957844ef7b17154b9e3",
+    "url": "/react-portfolio/static/media/css.606ea0c8.svg"
+  },
+  {
+    "revision": "873649e2067578074907c23bc3b8864a",
+    "url": "/react-portfolio/static/media/java.873649e2.svg"
+  },
+  {
+    "revision": "acb82063c63b211634ac8fc6a42ac4e5",
+    "url": "/react-portfolio/static/media/javascript.acb82063.svg"
+  },
+  {
+    "revision": "6d3c2e50a6ebd417f6a993950dc162a5",
+    "url": "/react-portfolio/static/media/left-arrow.6d3c2e50.svg"
+  },
+  {
+    "revision": "df98a55a8c326804a215312b3694e2aa",
+    "url": "/react-portfolio/static/media/postgresql.df98a55a.svg"
+  },
+  {
+    "revision": "340e5a9cf4a0c4214802126b328b9885",
+    "url": "/react-portfolio/static/media/react.340e5a9c.svg"
+  },
+  {
+    "revision": "45918c69b219ce46078930bca605a576",
+    "url": "/react-portfolio/static/media/sass.45918c69.svg"
+  },
+  {
+    "revision": "26532e771f802581ce23a0a064e18182",
+    "url": "/react-portfolio/static/media/spring.26532e77.svg"
+  },
+  {
+    "revision": "ca269c4a366a593a835566ea80368032",
+    "url": "/react-portfolio/static/media/typescriptlang.ca269c4a.svg"
+  }
+]);
