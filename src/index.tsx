bb40ed79f3178cdcdac0493d17925966084372c7
Minong <minhk3259@gmail.com>
2019-08-01 13:34:31 +0900

    리덕스 크롬 익스텐션 옵션 추가

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,11 @@ import { createStore } from "redux";
 import reducers from "./modules";
 import { Provider } from "react-redux";
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
