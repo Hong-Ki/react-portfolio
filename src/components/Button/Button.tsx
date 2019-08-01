f2d57dd5baf105f80c8c7d7cf87f08f3515e96bf
Minong <minhk3259@gmail.com>
2019-08-01 13:36:22 +0900

    Type추가 및 스타일 변경

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import styles from "./Button.module.scss";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
-const Button: React.SFC = props => {
-  return <div>{props.children}</div>;
+
+interface Props {
+  children?: ReactNode;
+  isVisible?: boolean;
+  onClick?(): void;
+}
+
+const Button: React.SFC<Props> = ({ onClick, children }) => {
+  return <div>{children}</div>;
 };
-export const FloatingButton: React.SFC = props => {
+export const FloatingButton: React.SFC<Props> = ({
+  onClick,
+  children,
+  isVisible
+}) => {
   return (
-    <div className={cx("floating")}>
-      <div className={cx("bar", "origin")} />
+    <div className={cx("floating", { close: isVisible })} onClick={onClick}>
+      <div className={cx("bar")} />
     </div>
   );
 };
