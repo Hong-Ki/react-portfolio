11a4640a50f3f0ea5a619c9f007dd31808d46092
Minong <minhk3259@gmail.com>
2019-08-01 13:37:49 +0900

    Type정의 및 스타일 정리

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,30 +1,31 @@
-import React from "react";
-import styles from "./Main.module.scss";
-import Page from "../Page/Page";
-import classNames from "classnames/bind";
+import React, { FC } from 'react';
+import styles from './Main.module.scss';
+import Page from '../Page/Page';
+import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const Main: React.FC = props => {
+interface Props {
+  id?: string;
+  current: string;
+}
+
+const Main: FC<Props> = ({ id, current }) => {
   return (
-    <Page>
-      <div className={cx("back")}>
-        <span>{"PORT"}</span>
-        <span>{"FOLIO"}</span>
-      </div>
-      <div className={cx("top")}>
-        <div className={cx("box", "left")}>
-          <span className={cx("blur")}>Hello,</span>
-          <span className={cx("blur")}>I am</span>
+    <Page backgroundText={['PORT', 'FOLIO']} id={id} current={current}>
+      <div className={cx('top')}>
+        <div className={cx('box', 'left')}>
+          <span className={cx('blur')}>Hello,</span>
+          <span className={cx('blur')}>I am</span>
         </div>
-        <div className={cx("box", "right")}>
-          <span className={cx("big", "neon")}>Hong-ki Min</span>
-          <span className={cx("neon")}>Web Developer</span>
+        <div className={cx('box', 'right')}>
+          <span className={cx('neon', 'big')}>Hong-ki Min</span>
+          <span className={cx('neon')}>Web Developer</span>
         </div>
       </div>
-      <div className={cx("bottom")}>
-        <span className={cx("blur")}>Welcom to my</span>
-        <span className={cx("neon", "big")}>PORTFOLIO SITE</span>
+      <div className={cx('bottom', 'box')}>
+        <span className={cx('blur')}>Welcom to</span>
+        <span className={cx('neon', 'big')}>PORTFOLIO SITE</span>
       </div>
     </Page>
   );
